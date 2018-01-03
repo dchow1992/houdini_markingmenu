51bae5aaf73470f295339051656c364322f4b3d0
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-01-03 00:05:29 -0800

    Add files via upload

--- ne_mm_libs/NE_markingMenuEditor.py
+++ ne_mm_libs/NE_markingMenuEditor.py
@@ -1,13 +1,9 @@
 import sys, hou, os, json
 #on work machines use this line
-from PySide import QtGui, QtCore, QtTest
-#from PySide2 import QtGui, QtGui, QtCore, QtTest
+from PySide2 import QtWidgets, QtGui, QtCore, QtTest
+#from PySide2 import QtWidgets, QtWidgets, QtCore, QtTest
 
-class cbStatus(QtCore.QObject):
-    checked = QtCore.Signal()
-    unchecked = QtCore.Signal()
-
-class NE_MarkingMenuEditor(QtGui.QWidget):    
+class NE_MarkingMenuEditor(QtWidgets.QWidget):    
     def __init__(self):
         super(NE_MarkingMenuEditor, self).__init__()
         
@@ -25,124 +21,151 @@ class NE_MarkingMenuEditor(QtGui.QWidget):
         self.setWindowTitle('Marking Menu Editor')
         self.setGeometry(300,300, 900, 700)
         self.setStyleSheet('background-color: rgb(58,58,58);')
-        self.setMinimumSize(1200, 700)
+        self.setMinimumSize(1400, 700)
         
         #root layout
-        self.vlayout0 = QtGui.QVBoxLayout()
+        self.vlayout0 = QtWidgets.QVBoxLayout()
         self.setLayout(self.vlayout0)
 
         #collection combo box
         self.collectComboUI()
         
-        #separator
+        #spacer
         self.vlayout0.addSpacing(10)
         
         #collection tree widget
         self.treeWidgetUI()
         
-        #separator
+        #spacer
         self.vlayout0.addSpacing(10)
         
+        #details pane
+        self.detailsUI()
+
+        #draw default collection, SOP_baseCollection.collection in this case
+        self.updateTree(self.treeWidget, 'SOP_baseCollection.collection')
+
+        self.run()
+        
+    def detailsUI(self):
         #collection buttons
-        self.glayout0 = QtGui.QGridLayout()
+        self.glayout0 = QtWidgets.QGridLayout()
         self.glayout0.setSpacing(10)
         self.vlayout0.addLayout(self.glayout0)
-        
+
         #row labels
         self.activeCheckWidgets = []
         self.menuCheckWidgets = []
         self.labelInputWidgets = []
         self.iconInputWidgets = []
         self.collectComboWidgets = []
+        self.commandLabelWidgets = []
         self.commandComboWidgets = []
         self.nodeInputWidgets = []
         self.activeWireCheckWidgets = []
-        
+
         #all enabled checkbox
         self.allEnabledWidget = hou.qt.createCheckBox()
         self.glayout0.addWidget(self.allEnabledWidget, 0, 0, 1, 1)
         self.allEnabledWidget.stateChanged.connect(self.allEnabled)
-        
+
         #all menu checkbox
         self.allMenuWidget = hou.qt.createCheckBox()
         self.glayout0.addWidget(self.allMenuWidget, 0, 1, 1, 1)
         self.allMenuWidget.stateChanged.connect(self.allMenus)
-        
+
         #all active wire on create checkbox
         self.allActiveWireWidget = hou.qt.createCheckBox()
         self.glayout0.addWidget(self.allActiveWireWidget, 0, 8, 1, 1)
         self.allActiveWireWidget.stateChanged.connect(self.allActiveWires)
-        
+
+        #separator
+        self.detailSeparatorWidget = hou.qt.createSeparator()
+        self.detailSeparatorWidget.setStyleSheet(hou.qt.styleSheet())
+        self.glayout0.addWidget(self.detailSeparatorWidget,1,0, 1, 10)
+
+        #header labels
+        labels = ['ITEM LABEL', 'ICON', 'MENU COLLECTION', 'BUTTON FUNC','BUTTON ARG']
+        labelcolumns = [2, 3, 4, 6, 7]
+        for x in labels:
+            label = QtWidgets.QLabel(x)
+            font = QtGui.QFont()
+            font.setBold(True)
+            label.setFont(font)        
+            self.glayout0.addWidget(label, 0, labelcolumns[labels.index(x)], 1, 1)
+
         #for each button (8 max per collection)
-        for i in range(8):       
+        for i in range(2, 8+2):       
             #enabled checkboxes
             h = hou.qt.createCheckBox()
-            h.setText('Item %d' % i)    
+            h.setText('Item %d' % (i-2))    
             h.setCheckState(QtCore.Qt.CheckState.Checked)
             self.activeCheckWidgets.append(h)           
-            self.glayout0.addWidget(h, i+1, 0, 1, 1)            
-            
+            self.glayout0.addWidget(h, i, 0, 1, 1)            
+            h.stateChanged.connect(self.updateDetailsUI)
+
             #is menu checkboxes
             a = hou.qt.createCheckBox()            
             a.setText('Menu')
             self.menuCheckWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 1, 1, 1)
-            #a.stateChanged.connect(self.hideCollectCombo)
-            
+            self.glayout0.addWidget(a, i, 1, 1, 1)            
+            a.stateChanged.connect(self.updateMenuDetailWidgets)
+
             #label line edits
-            a = QtGui.QLineEdit()
+            a = QtWidgets.QLineEdit()
             a.setPlaceholderText('Label')
             self.labelInputWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 2, 1, 1)
+            self.glayout0.addWidget(a, i, 2, 1, 1)
             
             #icon line edits
-            a = QtGui.QLineEdit()
-            a.setPlaceholderText('Icon - for example: MISC_python')
+            a = QtWidgets.QLineEdit()
+            a.setPlaceholderText('Icon - ex: MISC_python')
             self.iconInputWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 3, 1, 1)
+            self.glayout0.addWidget(a, i, 3, 1, 1)
             
             #point to collection if menu combo boxes
-            a = QtGui.QComboBox()
-            a.insertItem(0, 'Set Collection')
+            a = QtWidgets.QComboBox()
+            a.insertItem(0, 'Link Collection')
             a.insertItems(1, [x for x in self.collections if x != self.collectCombo.currentText()])
+            a.setDisabled(True)
             self.collectComboWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 4, 1, 1)
+            self.glayout0.addWidget(a, i, 4, 1, 1)
             
             #command labels
-            a = QtGui.QLabel('Command: ')
-            self.glayout0.addWidget(a, i+1, 5, 1, 1)
+            a = QtWidgets.QLabel('Command: ')
+            self.commandLabelWidgets.append(a)
+            self.glayout0.addWidget(a, i, 5, 1, 1)
             
             #command type combo boxes
-            a = QtGui.QComboBox()
+            a = QtWidgets.QComboBox()
             a.insertItem(0, 'Command Type')
             a.insertItems(1, ['Create Node', 'Custom Function'])
             self.commandComboWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 6, 1, 1)
-            
+            self.glayout0.addWidget(a, i, 6, 1, 1)
+            a.currentIndexChanged.connect(self.toggleTextHint)
+            a.currentIndexChanged.connect(self.disableActiveWire)
+
             #nodename or custom command function line edits
-            a = QtGui.QLineEdit()
+            a = QtWidgets.QLineEdit()
             a.setPlaceholderText('nodename')
             self.nodeInputWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 7, 1, 1)
+            self.glayout0.addWidget(a, i, 7, 1, 1)
             
             #active wire checkboxes
             a = hou.qt.createCheckBox()            
             a.setText('Active Wire On Create')
             self.activeWireCheckWidgets.append(a)
-            self.glayout0.addWidget(a, i+1, 8, 1, 1)
-            h.stateChanged.connect(a.setDisabled)
+            self.glayout0.addWidget(a, i, 8, 1, 1)
             
-        self.run()
-        
     def collectComboUI(self):
         #collections combo box
-        self.hlayout0 = QtGui.QHBoxLayout()
+        self.hlayout0 = QtWidgets.QHBoxLayout()
         self.hlayout0.setSpacing(0)
         self.vlayout0.addLayout(self.hlayout0)
         
-        self.collectComboLabel = QtGui.QLabel('Active Collection: ', self)
+        self.collectComboLabel = QtWidgets.QLabel('Active Collection: ', self)
         self.collectComboLabel.setAlignment(QtCore.Qt.AlignLeft)
-        self.collectCombo = QtGui.QComboBox(self)
+        self.collectCombo = QtWidgets.QComboBox(self)
         self.collectCombo.setMinimumContentsLength(35)
         
         defaultCollection = 'SOP_baseCollection.collection'
@@ -165,23 +188,117 @@ class NE_MarkingMenuEditor(QtGui.QWidget):
         
     def treeWidgetUI(self):
         #tree view init
-        self.treeWidget = QtGui.QTreeWidget(self)
+        self.treeWidget = QtWidgets.QTreeWidget(self)
         self.treeWidget.setColumnCount(1)
         self.treeWidget.setHeaderLabel('Marking Menu Collection Overview')   
         self.treeWidget.setItemsExpandable(False)
         self.treeWidget.setStyleSheet('QTreeView::branch:open { image: url(none.png); } ')        
-        self.vlayout0.addWidget(self.treeWidget)               
+        self.vlayout0.addWidget(self.treeWidget) 
+
+    def clearDetailRow(self, idx):
+        #set a row to default values
+        self.activeCheckWidgets[idx].setCheckState(QtCore.Qt.Unchecked)
+        self.menuCheckWidgets[idx].setCheckState(QtCore.Qt.Unchecked)        
+        self.labelInputWidgets[idx].clear()
+        self.iconInputWidgets[idx].clear()
+        self.collectComboWidgets[idx].setCurrentIndex(0)
+        self.collectComboWidgets[idx].setDisabled(True)
+        self.commandComboWidgets[idx].setCurrentIndex(0)
+        self.nodeInputWidgets[idx].clear()
+        self.activeWireCheckWidgets[idx].setCheckState(QtCore.Qt.Unchecked)
+
+    def loadDetailsFromCollection(self, collection, idx):
+        self.clearDetailRow(idx)        
+        self.activeCheckWidgets[idx].setCheckState(QtCore.Qt.Checked)
+        self.labelInputWidgets[idx].setText(collection['label'])
+        self.iconInputWidgets[idx].setText(collection['icon'])
+
+        if collection['isMenu']:
+            self.menuCheckWidgets[idx].setCheckState(QtCore.Qt.Checked)
+
+            0 if collection['menuCollection'] == '' else \
+                 self.collectComboWidgets[idx].setCurrentText(self.collectComboWidgets[idx].findText(collection['menuCollection']))
         
-        self.updateTree(self.treeWidget, 'SOP_baseCollection.collection') 
-        
-        
-        
+        else:
+            if collection['commandType'] == 'createnode':
+                self.commandComboWidgets[idx].setCurrentIndex(1)
+                self.nodeInputWidgets[idx].setText(collection['nodetype'])
+
+            if collection['activeWire']:
+                self.activeWireCheckWidgets[idx].setCheckState(QtCore.Qt.Checked)
+
+            elif collection['command'] == '' and collection['commandType'] != 'createnode':
+                self.commandComboWidgets[idx].setCurrentIndex(0)
+
+            elif collection['command'] != '' and collection['commandType'] != 'createnode':
+                self.commandComboWidgets[idx].setCurrentIndex(2)
+                self.nodeInputWidgets[idx].setText(collection['command'])
+
+    
+    def updateDetailsUI(self):
+        for cb in self.activeCheckWidgets:
+            idx = self.activeCheckWidgets.index(cb)
+            if cb.checkState() != QtCore.Qt.Checked:
+                cb.setTextColor(QtGui.QColor(131, 131, 131))
+                self.menuCheckWidgets[idx].setDisabled(True)
+                self.labelInputWidgets[idx].setDisabled(True)
+                self.iconInputWidgets[idx].setDisabled(True)
+                self.collectComboWidgets[idx].setDisabled(True)
+                self.commandLabelWidgets[idx].setDisabled(True)
+                self.commandComboWidgets[idx].setDisabled(True)
+                self.nodeInputWidgets[idx].setDisabled(True)
+                self.activeWireCheckWidgets[idx].setDisabled(True)
+            else:
+                cb.setTextColor(QtGui.QColor(203, 203, 203))
+                self.menuCheckWidgets[idx].setDisabled(False)
+                self.labelInputWidgets[idx].setDisabled(False)
+                self.iconInputWidgets[idx].setDisabled(False)                
+                self.collectComboWidgets[idx].setDisabled(False)
+                self.commandLabelWidgets[idx].setDisabled(False)
+                self.commandComboWidgets[idx].setDisabled(False)
+                self.nodeInputWidgets[idx].setDisabled(False)
+                self.activeWireCheckWidgets[idx].setDisabled(False)
+        self.updateMenuDetailWidgets()
+
+    def updateMenuDetailWidgets(self):
+        for cb in self.menuCheckWidgets:
+            idx = self.menuCheckWidgets.index(cb)            
+            if cb.checkState() != QtCore.Qt.Checked and cb.isEnabled():
+                self.collectComboWidgets[idx].setDisabled(True)
+                self.commandLabelWidgets[idx].setDisabled(False)
+                self.commandComboWidgets[idx].setDisabled(False)
+                self.nodeInputWidgets[idx].setDisabled(False)
+                self.activeWireCheckWidgets[idx].setDisabled(False)
+
+            elif cb.isEnabled() and cb.checkState() == QtCore.Qt.Checked:
+                self.collectComboWidgets[idx].setDisabled(False)
+                self.commandLabelWidgets[idx].setDisabled(True)
+                self.commandComboWidgets[idx].setDisabled(True)
+                self.nodeInputWidgets[idx].setDisabled(True)
+                self.activeWireCheckWidgets[idx].setDisabled(True)
+
+    def toggleTextHint(self):
+        for cb in self.commandComboWidgets:
+            idx = self.commandComboWidgets.index(cb)
+            if cb.currentIndex() == 2:
+                self.nodeInputWidgets[idx].setPlaceholderText('function()')
+            else:
+                self.nodeInputWidgets[idx].setPlaceholderText('nodename')
+
+    def disableActiveWire(self):
+        for cb in self.commandComboWidgets:
+            idx = self.commandComboWidgets.index(cb)
+            if cb.isEnabled() and cb.currentIndex() == 1:
+                self.activeWireCheckWidgets[idx].setDisabled(False) 
+            else:
+                self.activeWireCheckWidgets[idx].setDisabled(True)
+
     def selectCollection(self):        
         self.updateTree(self.treeWidget, self.collectCombo.currentText())
         
     def allEnabled(self):
         for cb in self.activeCheckWidgets:            
-            cb.setCheckState(self.allEnabledWidget.checkState())
+            cb.setCheckState(self.allEnabledWidget.checkState())            
             
     def allMenus(self):
         for cb in self.menuCheckWidgets:
@@ -196,10 +313,10 @@ class NE_MarkingMenuEditor(QtGui.QWidget):
     def updateTree(self, parentItem, collectionFile):
         #load collection from disk, build parent item, populate item
         loaded = self.loadCollection(collectionFile)
-        
         self.treeWidget.clear()
         
-        collectionHeader = QtGui.QTreeWidgetItem(parentItem) 
+        #parent collection item
+        collectionHeader = QtWidgets.QTreeWidgetItem(parentItem) 
         collectionHeader.setText(0, collectionFile.split('.collection')[0]) 
         collectionHeader.setExpanded(True)
         collectionHeader.setIcon(0, hou.qt.createIcon('SOP_object_merge'))
@@ -208,34 +325,50 @@ class NE_MarkingMenuEditor(QtGui.QWidget):
         brush.setColor(QtGui.QColor(255, 178, 45))
         collectionHeader.setForeground(0, brush)
         
-        for i in range(len(loaded)):
+        #for each dict in current collection
+        for i in range(len(loaded)):            
             item = loaded['menuItem%d'%i]
-            idx = item['index']    
             
-            x = QtGui.QTreeWidgetItem(collectionHeader)
+            #if item isn't empty
+            if item != None:
+                idx = item['index']
             
-            x.setIcon(0, hou.qt.createIcon(item['icon']))
-            
-            x.setText(0, item['label'])            
-            x.setExpanded(True)
-            
-            brush.setColor(QtGui.QColor(203, 203, 203))            
-            x.setForeground(0, brush)
-            
-            if item['isMenu']:
-                brush.setColor(QtGui.QColor(153, 255, 45))
-                x.setForeground(0, brush)                
-                
-                if item['menuCollection'] == '':
-                    y = QtGui.QTreeWidgetItem(x)
-                    y.setText(0, 'Empty Menu')
-                    y.setIcon(0, hou.qt.createIcon('SOP_delete'))
-    
-                    brush.setColor(QtGui.QColor(153, 153, 153))
-                    y.setForeground(0, brush)
-                    y.setExpanded(True)
+                #tree display button label
+                x = QtWidgets.QTreeWidgetItem(collectionHeader)                
+                x.setIcon(0, hou.qt.createIcon(item['icon']))                
+                x.setText(0, '%d'%i + '. ' + item['label'])            
+                x.setExpanded(True)                
+                brush.setColor(QtGui.QColor(203, 203, 203))            
+                x.setForeground(0, brush)
+
+                self.loadDetailsFromCollection(item, idx)
+
+                if item['isMenu']:
+                    brush.setColor(QtGui.QColor(153, 255, 45))
+                    x.setForeground(0, brush)                
+                    
+                    if item['menuCollection'] == '':
+                        y = QtWidgets.QTreeWidgetItem(x)
+                        y.setText(0, 'No Collection Linked')
+                        y.setIcon(0, hou.qt.createIcon('SOP_delete'))
+        
+                        brush.setColor(QtGui.QColor(153, 153, 153))
+                        y.setForeground(0, brush)
+                        y.setExpanded(True)
+
+            else:
+                #add empty tree item
+                x = QtWidgets.QTreeWidgetItem(collectionHeader)                
+                x.setText(0, '%d'%i + '. ' + 'empty')            
+                x.setExpanded(True)                
+                brush.setColor(QtGui.QColor(120, 120, 120))            
+                x.setForeground(0, brush)
+
+                #empty detail row
+                self.clearDetailRow(i)
 
         
+      
     def updateCollections(self):
         #set class variable to a list of .collection files found in collectionsDir
         self.collections = [a for a in os.listdir(self.collectionsDir) if a.endswith('.collection')]
@@ -264,9 +397,9 @@ class NE_MarkingMenuEditor(QtGui.QWidget):
         
         
         
-print hou.qt.styleSheet()
-for entry in QtGui.qApp.allWidgets():
+#print hou.qt.styleSheet()
+for entry in QtWidgets.qApp.allWidgets():
     if type(entry).__name__ == 'NE_MarkingMenuEditor':
         entry.close()
         
-ex = NE_MarkingMenuEditor()
+#ex = NE_MarkingMenuEditor()
--- ne_mm_libs/collection_temp.py
+++ ne_mm_libs/collection_temp.py
@@ -1,12 +1,13 @@
 import json
 
 class buttonConfig:
-    def __init__(self, buttonType, nodetype, label, icon, index):
+    def __init__(self, buttonType, nodetype, label, icon, index, activeWire):
         self.itemType = buttonType
         self.nodetype = nodetype
         self.label = label
         self.icon = icon
         self.index = index
+        self.activeWire = activeWire
         self.config = {
             'itemType': self.itemType,
             'isMenu': True if buttonType == 'menu' else False,
@@ -14,48 +15,36 @@ class buttonConfig:
             'icon': self.icon,
             'index': self.index,        
             'menuObjects': {},
-            'command': 'cmds.createNode("%s", False)' % self.nodetype            
+            'menuCollection': '',
+            'nodetype': self.nodetype,
+            'commandType': 'createnode',
+            'activeWire': self.activeWire,
+            'command': 'cmds.createNode("%s", %s)' % (self.nodetype, self.activeWire)
         }
 
-menuItem0 = None
-menuItem1 = None
-menuItem2 = None
-menuItem3 = None
-menuItem4 = None
-menuItem5 = None
-menuItem6 = None
-menuItem7 = None
+outputFile = {}
+for i in range(8):
+    outputFile['menuItem%d'%i] = None
 
-menuItem0 = buttonConfig('button', 'null', 'Null', 'SOP_null', 0)
-menuItem1 = buttonConfig('button', 'attribwrangle', 'Attribute Wrangle', 'SOP_attribwrangle', 1)
-menuItem2 = buttonConfig('menu', 'null', 'Null', 'SOP_null', 2)
-menuItem3 = buttonConfig('button', 'null', 'Null', 'SOP_null', 3)
-menuItem4 = buttonConfig('button', 'null', 'Null', 'SOP_null', 4)
-menuItem5 = buttonConfig('button', 'null', 'Null', 'SOP_null', 5)
-menuItem6 = buttonConfig('button', 'null', 'Null', 'SOP_null', 6)
-menuItem7 = buttonConfig('button', 'null', 'Null', 'SOP_null', 7)
+outputFile['menuItem0'] = buttonConfig('button', 'vdbfrompolygons', 'VDB from Polygons', 'SOP_OpenVDB', 0, False).config
+outputFile['menuItem1'] = buttonConfig('button', 'bound', 'Bound', 'SOP_bound', 1, False).config
+outputFile['menuItem2'] = buttonConfig('button', 'box', 'Box', 'SOP_box', 2, False).config
+outputFile['menuItem3'] = buttonConfig('button', 'null', 'Null', 'SOP_null', 3, False).config
+outputFile['menuItem4'] = buttonConfig('button', 'attribrename', 'Attribute Rename', 'SOP_attribute', 4, False).config
+outputFile['menuItem5'] = buttonConfig('menu', 'attribdelete', 'Attribute Delete', 'SOP_attribdelete', 5, False).config
+outputFile['menuItem6'] = buttonConfig('button', 'attribwrangle', 'Vertex Wrangle', 'SOP_attribwrangle', 6, False).config
+outputFile['menuItem7'] = buttonConfig('button', 'normal', 'Normal', 'SOP_normal', 7, False).config
 
 inputFile = {}
 
-outputFile = {
-    'menuItem0': menuItem0.config if menuItem0 != None else None,
-    'menuItem1': menuItem1.config if menuItem1 != None else None,
-    'menuItem2': menuItem2.config if menuItem2 != None else None,
-    'menuItem3': menuItem3.config if menuItem3 != None else None,
-    'menuItem4': menuItem4.config if menuItem4 != None else None,
-    'menuItem5': menuItem5.config if menuItem5 != None else None,
-    'menuItem6': menuItem6.config if menuItem6 != None else None,
-    'menuItem7': menuItem7.config if menuItem7 != None else None
-}
-
-filename = 'SOP_wrangles.collection'
+filename = 'SOP_baseCollection.collection'
 dirpath = hou.getenv('HOUDINI_USER_PREF_DIR')+'/python2.7libs/ne_mm_libs/collections/'
 
 with open(dirpath+filename, 'w') as f:
     json.dump(outputFile, f)
-
+'''
 with open(dirpath+filename, 'r') as f:
-    inputFile = json.load(f)
+    inputFile = json.load(f)'''
 
 #import pprint
 #pprint.pprint(inputFile)
