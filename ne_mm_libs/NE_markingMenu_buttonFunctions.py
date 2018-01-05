19b5a87cfb4f897a2e55a6fef9e8c7b34da4623a
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-01-04 22:34:09 -0800

    Add files via upload

--- ne_mm_libs/NE_markingMenu_buttonFunctions.py
+++ ne_mm_libs/NE_markingMenu_buttonFunctions.py
@@ -1,4 +1,7 @@
 import hou
+from ne_mm_libs import NE_markingMenuEditor as mm           
+from PySide2 import QtWidgets
+reload(mm)
 
 def createNode(node_name, activeWire):
 	network_editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
@@ -10,7 +13,20 @@ def createNode(node_name, activeWire):
 	node = network_editor.pwd().createNode(node_name)
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)
-	
+	'''
+	if 1:
+		data = {
+		        'connection':False,
+		        'inputitem':None,
+		        'inputindex':0,
+		        'outputitem':node,
+		        'outputindex':0,
+		        'branch':False,
+		        'nodetypename':None
+		        }
+
+		network_editor.pushEventContext('nodegraphactivewire', data)
+		node.setInput(0, data['inputitem'], data['inputindex'])'''
 	parm_view.setCurrentNode(node)
 
 def createPointWrangle():
@@ -24,6 +40,7 @@ def createPointWrangle():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)
 	node.parm('class').set(2)	
+	node.setName('pointwrangle1', True)
 	parm_view.setCurrentNode(node)
 
 def createPrimitiveWrangle():
@@ -36,7 +53,8 @@ def createPrimitiveWrangle():
 	node = network_editor.pwd().createNode('attribwrangle')
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)
-	node.parm('class').set(1)	
+	node.parm('class').set(1)
+	node.setName('primitivewrangle1', True)
 	parm_view.setCurrentNode(node)
 
 def createDetailWrangle():
@@ -50,6 +68,7 @@ def createDetailWrangle():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)
 	node.parm('class').set(0)	
+	node.setName('detailwrangle1', True)
 	parm_view.setCurrentNode(node)
 
 def createVertexWrangle():
@@ -63,6 +82,7 @@ def createVertexWrangle():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)
 	node.parm('class').set(3)	
+	node.setName('vertexwrangle1', True)
 	parm_view.setCurrentNode(node)
 
 def createVolumeWrangle():
@@ -87,7 +107,8 @@ def createPointVOP():
 	node = network_editor.pwd().createNode('attribvop')
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)	
-	node.parm('bindclass').set(2)	
+	node.parm('bindclass').set(2)
+	node.setName('pointvop1', True)	
 	parm_view.setCurrentNode(node)
 
 def createPrimitiveVOP():
@@ -101,6 +122,7 @@ def createPrimitiveVOP():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)	
 	node.parm('bindclass').set(1)	
+	node.setName('primitivevop1', True)
 	parm_view.setCurrentNode(node)
 
 def createDetailVOP():
@@ -114,6 +136,7 @@ def createDetailVOP():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)	
 	node.parm('bindclass').set(0)	
+	node.setName('detailvop1', True)
 	parm_view.setCurrentNode(node)
 
 def createVertexVOP():
@@ -127,6 +150,7 @@ def createVertexVOP():
 	node.move(position)
 	node.setSelected(True, clear_all_selected=True)	
 	node.parm('bindclass').set(3)	
+	node.setName('vertexvop1', True)
 	parm_view.setCurrentNode(node)
 
 def createVolumeVOP():
@@ -137,6 +161,55 @@ def createVolumeVOP():
 	position = network_editor.selectPosition() 
 
 	node = network_editor.pwd().createNode('volumevop')
-	node.move(position)
+	node.move(position)	
 	node.setSelected(True, clear_all_selected=True)			
 	parm_view.setCurrentNode(node)
+
+
+
+def openEditor():
+	for entry in QtWidgets.qApp.allWidgets():
+	    if type(entry).__name__ == 'NE_MarkingMenuEditor':
+	        entry.setParent(None)
+	        entry.close()	        
+	ex = mm.NE_MarkingMenuEditor()
+
+def mergeSelection():
+	x = hou.selectedNodes()
+
+	if(len(x) > 0):
+		network_editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
+		parm_view = hou.ui.paneTabOfType(hou.paneTabType.Parm)
+
+		position = network_editor.selectPosition()
+		position = network_editor.selectPosition() 
+
+		a = network_editor.pwd().createNode("merge")
+
+		a.setPosition(position)
+
+		for node in x:
+		    a.setNextInput(node)
+
+		network_editor.setCurrentNode(a)
+
+		a.setSelected(True, clear_all_selected=1)
+
+def objMergeSelection():
+	network_editor = hou.ui.paneTabOfType(hou.paneTabType.NetworkEditor)
+	parm_view = hou.ui.paneTabOfType(hou.paneTabType.Parm)
+
+	pos = network_editor.selectPosition()
+	pos = network_editor.selectPosition()
+
+	if(len(hou.selectedNodes()) > 0):
+		x = hou.selectedNodes()[0]        
+
+		a = network_editor.pwd().createNode("object_merge", "ref_"+x.name())        
+
+		a.parm('objpath1').set(x.path())
+	else:
+		a = network_editor.pwd().createNode("object_merge")
+	a.setPosition(pos)
+	network_editor.setCurrentNode(a)
+	a.setSelected(True, clear_all_selected=1)
