c09fa43e15bc4340020d8f4cf7232b3e77b99564
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-10-01 16:05:16 -0700

    Update nodegraphhooks.py

--- nodegraphhooks.py
+++ nodegraphhooks.py
@@ -1,8 +1,24 @@
+import os
+ 
+import sys
+ 
 import hou
-from nemarkingmenu import markingmenu as mm, markingmenunodegraph as nodegraph
+ 
+from houdini_markingmenu import markingmenu as mm
+ 
+from houdini_markingmenu import markingmenunodegraph as nodegraph
+ 
 from canvaseventtypes import *
-
-
+ 
+import nodegraphbase as base
+ 
+sys.path.insert(0, os.path.join(hou.getenv('HOUDINI_USER_PREF_DIR'),
+                                'python2.7libs',
+                                'houdini_markingmenu'))
+ 
+import buttonfunctions as cmds
+ 
+ 
 class MarkingMenuMouseHandler(nodegraph.NodeMouseHandler):
     def handleEvent(self, uievent, pending_actions):
         if isinstance(uievent, MouseEvent) and uievent.eventtype == 'mousedrag':
@@ -11,23 +27,27 @@ class MarkingMenuMouseHandler(nodegraph.NodeMouseHandler):
             # end custom event handling, return None
             # Otherwise return traditional nodegraph rmb handling
             return None
-
+ 
         return nodegraph.NodeMouseHandler.handleEvent(
             self, uievent, pending_actions)
-
-
+ 
+ 
 # We catch the mousedown event and then further handling is done by our custom class
 # createEventHandler catches mouse events and either does stuff
 # and/or returns an eventHandler class with a handleEvent() method that
 # handles the events until completion of the event or hands it off to a different handler
-
-
+ 
+ 
 def createEventHandler(uievent, pending_actions):
-    if isinstance(uievent, MouseEvent) and \
-       uievent.eventtype == 'mousedown' and \
-       uievent.mousestate.rmb:
-        return MarkingMenuMouseHandler(uievent), True
+    if isinstance(uievent, MouseEvent) and uievent.eventtype == 'mousedown' and uievent.mousestate.rmb:
+ 
+        reload(cmds)
+ 
+        if cmds.menu_activate == 1:
+            return MarkingMenuMouseHandler(uievent), True
+        else:
+            return base.ViewScaleHandler(uievent), True
     if isinstance(uievent, MouseEvent) and uievent.eventtype == 'mousedown':
         None
-
+ 
     return None, False
