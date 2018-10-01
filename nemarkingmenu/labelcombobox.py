8d30c81011ae12546db1299bef193e1c7275a898
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-10-01 16:31:24 -0700

    Create labelcombobox.py

---
+++ nemarkingmenu/labelcombobox.py
@@ -0,0 +1,24 @@
+import hou
+ 
+from Qt_py.Qt import QtWidgets
+ 
+ 
+class LabelComboBox(QtWidgets.QWidget):
+    def __init__(self, name, minxsize):
+        super(LabelComboBox, self).__init__()
+        self.name = name
+        self.minxsize = minxsize
+        self.initUI()
+ 
+    def initUI(self):
+        self.layout = QtWidgets.QHBoxLayout()
+        self.label = QtWidgets.QLabel(self.name)
+ 
+        self.comboBox = hou.qt.createComboBox()
+        self.comboBox.setMinimumSize(self.minxsize, 0)
+        self.comboBox.setStyleSheet(hou.qt.styleSheet())
+ 
+        self.layout.addWidget(self.label)
+        self.layout.addSpacing(5)
+        self.layout.addWidget(self.comboBox)
+        self.layout.addStretch(1)
