ee1d2be98224ac0eeb6042afb0ef1d806b391da1
dchow1992 <34880730+dchow1992@users.noreply.github.com>
2018-10-01 16:34:33 -0700

    Create editortaskbar.py

---
+++ nemarkingmenu/editortaskbar.py
@@ -0,0 +1,31 @@
+import hou
+ 
+from Qt_py.Qt import QtWidgets
+ 
+ 
+class EditorTaskbar(QtWidgets.QWidget):
+    def __init__(self):
+        super(EditorTaskbar, self).__init__()
+        self.initUI()
+ 
+    def initUI(self):
+        self.layout = QtWidgets.QHBoxLayout()
+        self.layout.addStretch(1)
+ 
+        self.saveTextWidget = QtWidgets.QLabel()
+        self.saveTextWidget.setStyleSheet('font: 18pt;')
+ 
+        self.layout.addWidget(self.saveTextWidget)
+        self.layout.addStretch(1)
+ 
+        self.saveButton = QtWidgets.QPushButton('Save Collection')
+        self.closeButton = QtWidgets.QPushButton('Close')
+ 
+        self.saveButton.setMinimumSize(220, 70)
+        self.closeButton.setMinimumSize(220, 70)
+ 
+        self.saveButton.setStyleSheet(hou.qt.styleSheet())
+        self.closeButton.setStyleSheet(hou.qt.styleSheet())
+ 
+        self.layout.addWidget(self.saveButton)
+        self.layout.addWidget(self.closeButton)
